41044647fcf89714b1688a18f963fcebee982886
William Oakes <oakesice@yahoo.com>
2019-04-09 02:35:53 +0000

    Encryption

--- Pa1.c
+++ Pa1.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 
-
+int nCrypt(int x,int y);
+int dCrypt(int x,int y);
 
 int main(void)
 {
     int a,b;
-    char d[]= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char c;
+   // char d[]= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   
+    char d[]= "";
+    char c = 'a';
+    int key = 5; 
+   
+    //printf("Enter a letter to encrypt: ");
+   
+    //scanf("%c", &c);
     
-    printf("Enter a letter or phrase (UPPERCASE only) to encrypt this: ");
-    scanf("%c",&c);
+    int counter = 0; 
     
-    c = c - 65; //Converts ASCII starting from 0 to 25
     
-    printf("%c %c",d[2]);
+    for(counter  = 0; counter<; counter++)
+    {
+        d[counter] = d[counter] + key; 
+    }
+    
+    printf("%s\n", d); 
+   
+   c = c + key; 
+   
+   //a = nCrypt(int x,int y);
+   
+   printf("%c\n",c);
+   
+   // printf("%c %c",d[c],d[c+1]);
     return 0;
     
 }
 
+/*int nCrypt(int x, int y){
+ //c = c - 65; //Converts ASCII letters to 0 for A, 1 for B ....
+ 
+ nCrypt = (x + y)(%26);
+ 
+ return nCrypt;
+ }*/
