b3d73aaf613167a6ed37de29d0a6d6220ebc5efe
William Oakes <oakesice@yahoo.com>
2019-04-28 02:06:19 +0000

    changed key, find function for uppercase and decryption

--- Pa1.c
+++ Pa1.c
@@ -8,28 +8,25 @@ int main(void)
 {
     int a,b;   
     char d[150];
-    char c = 'a';
-    int key = 19; //fixed for small letters correctly turned into big letters
+    int key = 2; //different key
    
      printf("Enter a letter to encrypt: ");
    
      scanf(" %[^\n]s", d); //[^\n] ignores the whitespace
     
+    //Encryption process condition for lowercase letters turning into uppercase
     int counter = 0; 
     
     for(counter  = 0; counter<d[counter]; counter++) //for loop dependent on how many characters the user enters
     {
         if(d[counter]!=32) //condition for the ASCII code to ignore the space character
-        d[counter] = ((d[counter]-65) + key)%26 + 65;
-    }
+        d[counter] = ((d[counter]-65) + 19)%26 + 65; //equation in order to convert small characters to big characters. The number 19 is a value tested and worked for conversion
+    } 
     
     printf("%s\n", d); 
    
-   c = c + key; 
    
    //a = nCrypt(int x,int y);
-   
-   printf("%c\n",c);
    
    // printf("%c %c",d[c],d[c+1]);
     return 0;
