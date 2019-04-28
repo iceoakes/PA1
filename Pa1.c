e637e71077163fc7f61473f530462978c7a84842
William Oakes <oakesice@yahoo.com>
2019-04-28 04:44:39 +0000

    added options to make the code user friendly

--- Pa1.c
+++ Pa1.c
@@ -11,12 +11,44 @@ int main(void)
     char d[150];
     int key = 25; 
     int i=0;
-  
-     printf("Enter a letter to encrypt: ");
      
+     printf("Please enter a number for the choices below:\n");
+     printf(" Enter 0 for encryption with rotation cipher\n ");
+     printf("Enter 1 for decryption with rotation cipher\n ");
+     printf("Enter 2 to encrypt using substitution cipher\n ");
+     printf("Enter 3 to decrypt using substitution cipher\n ");
+     printf("Selection: ");
      
-     scanf(" %[^\n]s", d); //[^\n] ignores the whitespace
+     scanf("%d",&a);
+    
      
+     switch(a){
+        case 0:
+         printf("Enter a phrase to encrypt:");
+         scanf(" %[^\n]s", d);//[^\n] ignores the whitespace
+         
+     //for loop that converts lowercase letters into uppercase
+    for (i = 0; d[i]!='\0'; i++)
+   {
+             if(d[i] >= 'a' && d[i] <= 'z') 
+          d[i] = d[i] - 32;
+   }    
+   
+          //Encryption process
+    int counter =0; //counter initialised to zero in order to avoid errors
+    for(counter  = 0; counter < d[counter]; counter++) //a "for loop" that is dependent on how many characters the user enters
+    {
+             if(d[counter]!=32) //condition for the ASCII code to ignore the space character
+        d[counter] = ((d[counter]-65) + key)%26 + 65; //calculation to provide encoded letters shifted by 1 to the right
+        
+    } 
+    
+    printf("%s\n", d);
+    break;
+    
+        case 1:
+          printf("Enter a phrase to decrypt:");
+          scanf(" %[^\n]s", d);//[^\n] ignores the whitespace
      //for loop that converts lowercase letters into uppercase
    for (i = 0; d[i]!='\0'; i++)
    {
@@ -24,17 +56,16 @@ int main(void)
           d[i] = d[i] - 32;
    }    
    
-    //Encryption process
-    int counter =0;
-    for(counter  = 0; counter < d[counter]; counter++) //for loop dependent on how many characters the user enters
+     //Decryption process
+    for(counter  = 0; counter < d[counter]; counter++) //a "for loop" that is dependent on how many characters the user enters
     {
         if(d[counter]!=32) //condition for the ASCII code to ignore the space character
-        d[counter] = ((d[counter]-65) + key)%26 + 65; //calculation to provide encoded letters shifted by 1 to the right
-        
-    } 
-    
-    printf("%s\n", d);
-   
+        d[counter] = ((d[counter]+65) - key)%26 +65;
+    }
+    printf("%s\n",d);
+    break;
+
     return 0;
     
+}
 }
