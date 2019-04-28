2208865b4a8b023cba5f4a2f5f1fb11595544d4d
William Oakes <oakesice@yahoo.com>
2019-04-28 03:05:41 +0000

    found out a "for loop" to convert small letters to big letters

--- Pa1.c
+++ Pa1.c
@@ -1,36 +1,56 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int nCrypt(int x,int y);
 int dCrypt(int x,int y);
+void upperCase(char []);
 
 int main(void)
 {
     int a,b;   
     char d[150];
     int key = 2; //different key
-   
+    int i=0;
+  
      printf("Enter a letter to encrypt: ");
-   
+     
+     
      scanf(" %[^\n]s", d); //[^\n] ignores the whitespace
-    
-    //Encryption process condition for lowercase letters turning into uppercase
-    int counter = 0; 
-    
+     
+     //for loop that converts lowercase letters into uppercase
+        for (i = 0; d[i]!='\0'; i++) {
+      if(d[i] >= 'a' && d[i] <= 'z') {
+         d[i] = d[i] - 32;
+      }
+   }    
+   
+    //Encryption process
+    int counter =0;
     for(counter  = 0; counter<d[counter]; counter++) //for loop dependent on how many characters the user enters
     {
         if(d[counter]!=32) //condition for the ASCII code to ignore the space character
-        d[counter] = ((d[counter]-65) + 19)%26 + 65; //equation in order to convert small characters to big characters. The number 19 is a value tested and worked for conversion
+        d[counter] = ((d[counter]-65) + 19)%26 + 65; //
     } 
     
-    printf("%s\n", d); 
-   
+    printf("%s\n", d);
    
    //a = nCrypt(int x,int y);
    
    // printf("%c %c",d[c],d[c+1]);
+   
     return 0;
     
+}
+void upperCase(char s[]) {
+   int c = 0;
+   
+   while (s[c] != '\0') {
+      if (s[c] >= 'a' && s[c] <= 'z') {
+         s[c] = s[c] - 32;
+      }
+      c++;
+   }
 }
 
 /*int nCrypt(int x, int y){
