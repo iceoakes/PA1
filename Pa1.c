31eb634a78fd4e2f375130358bacc28160d24717
William Oakes <oakesice@yahoo.com>
2019-04-28 23:55:34 +0000

    final changes, no substitution

--- Pa1.c
+++ Pa1.c
@@ -2,20 +2,18 @@
 #include <string.h>
 #include <ctype.h>
 
-int nCrypt(char d[],char y){
+/*int nCrypt(char d[],char y){
     for(int i=0; i <26; i++){
         if(d[i] == y){
             return i;
     }
 }
-return -1;
-}
-
-char sub[26] = {'Q','A','Z','X','S','W','E','D','C','V','F','R','T','G','B','N','H','Y','U','J','M','K','I','L','O','P'}; //defined letters for substitution cipher
+*/
+char sub[26] = {'Q','A','Z','X','S','W','E','D','C','V','F','R','T','G','B','N','H','Y','U','J','M','K','I','L','O','P'}; //pre-defined letters for substitution cipher
 
 int main(void)
 {
-    int a,b;   
+    int a;
     char d[150];
     int key = 25; 
     int i=0;
@@ -24,15 +22,15 @@ int main(void)
      printf("Please enter a number for the choices below:\n");
      printf(" Enter 1 for encryption with rotation cipher\n ");
      printf("Enter 2 for decryption with rotation cipher\n ");
-     printf("Enter 3 to encrypt using substitution cipher\n ");
-     printf("Enter 4 to decrypt using substitution cipher\n ");
+     printf("Enter 3 for encryption using substitution cipher\n ");
+     printf("Enter 4 for decryption using substitution cipher\n ");
      printf("Selection: ");
      
-     scanf("%d",&a);//scans user input to put into switch statements
+     scanf("%d",&a);//scans the user input to put into switch statements
     
      
      switch(a){
-        case 1:
+        case 1: //when the user enters a 1.
          printf("Enter a phrase to encrypt:");
          scanf(" %[^\n]s", d);//[^\n] ignores the whitespace
          
@@ -45,6 +43,7 @@ int main(void)
    
          //Encryption process
     int counter =0; //counter initialised to zero in order to avoid errors
+    
     for(counter  = 0; counter < d[counter]; counter++) //a "for loop" that is dependent on how many characters the user enters
     {
              if(d[counter]!=32) //condition for the ASCII code to ignore the space character
@@ -58,6 +57,7 @@ int main(void)
         case 2:
           printf("Enter a phrase to decrypt:");
           scanf(" %[^\n]s", d);//[^\n] ignores the whitespace
+          
      //for loop that converts lowercase letters into uppercase
    for (i = 0; d[i]!='\0'; i++)
    {
